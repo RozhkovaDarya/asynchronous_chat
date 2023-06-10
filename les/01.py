972487ca3604b9631527816f9eda893f2f7462cf
Daria <mironochka1@gmail.com>
2023-06-10 18:02:12 +0400

    lesson_1

---
+++ les/01.py
@@ -0,0 +1,16 @@
+word_1 = "разработка"
+word_2 = "сокет"
+word_3 = "декоратор"
+
+print(word_1, type(word_1))
+print(word_2, type(word_2))
+print(word_3, type(word_3))
+print('-'*20)
+
+unicode_word_1 = '%u0440%u0430%u0437%u0440%u0430%u0431%u043E%u0442%u043A%u0430'
+unicode_word_2 = '%u0441%u043E%u043A%u0435%u0442'
+unicode_word_3 = '%u0434%u0435%u043A%u043E%u0440%u0430%u0442%u043E%u0440'
+
+print(unicode_word_1, type(unicode_word_1))
+print(unicode_word_2, type(unicode_word_2))
+print(unicode_word_3, type(unicode_word_3))
