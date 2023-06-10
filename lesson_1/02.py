df06acf16a12776372408d753e2c3873b9d8b5cb
Daria <mironochka1@gmail.com>
2023-06-10 16:27:48 +0400

    lesson_1 Exercise 2

---
+++ lesson_1/02.py
@@ -0,0 +1,9 @@
+word_1 = b"class"
+word_2 = b"function"
+word_3 = b"method"
+
+print(
+    word_1, type(word_1), len(word_1),
+    word_2, type(word_2), len(word_2),
+    word_3, type(word_3), len(word_3)
+      )
