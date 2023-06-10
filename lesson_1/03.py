a2c20c1f5ed45b5a529e753c9b3519058130189c
Daria <mironochka1@gmail.com>
2023-06-10 16:37:44 +0400

    lesson_1 Exercise 3

---
+++ lesson_1/03.py
@@ -0,0 +1,19 @@
+word_1 = b"attribute"
+print(word_1)
+
+word_2 = b"класс"
+print(word_2)
+# File "c:\Users\Mironochka\Desktop\Асинхронный чат\lessons\lesson_1\03.py", line 4
+#    word_2 = b"класс"
+#                          ^
+#SyntaxError: bytes can only contain ASCII literal characters
+
+word_3 = b"функция"
+print(word_3)
+#  File "c:\Users\Mironochka\Desktop\Асинхронный чат\lessons\lesson_1\03.py", line 11
+#     word_3 = b"функция"
+#                               ^
+# SyntaxError: bytes can only contain ASCII literal characters
+
+word_4 = b"type"
+print(word_4)
